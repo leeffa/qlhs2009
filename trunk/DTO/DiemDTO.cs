14f6a6d1c532730c3ecb377b4e4f9679b9bb259f
leeffa.it <leeffa.it@cc67d0b6-4d1c-11de-bba4-9112670d796e>
2009-05-31 08:54:15 +0000

    Sua DTO bang DIEM

--- trunk/DTO/DiemDTO.cs
+++ trunk/DTO/DiemDTO.cs
@@ -9,7 +9,10 @@ namespace DTO
         #region Attributes
         string m_mahs;
         string m_mamh;
-        int m_diem;
+        float m_diem15;
+        float m_diem1T;
+        float m_diemThi;
+        float m_diemTB;
         #endregion Attributes
 
         #region Properties
@@ -23,17 +26,37 @@ namespace DTO
             get { return m_mamh; }
             set { m_mamh = value; }
         }
-        public int Diem
+        public float Diem15
         {
-            get { return m_diem; }
-            set { m_diem = value; }
+            get { return m_diem15; }
+            set { m_diem15= value; }
         }
+        public float Diem1T
+        {
+            get { return m_diem1T; }
+            set { m_diem1T = value; }
+        }
+        public float DiemThi
+        {
+            get { return m_diemThi; }
+            set { m_diemThi = value; }
+        }
+        public float DiemTB
+        {
+            get { return m_diemTB; }
+            set { m_diemTB = value; }
+        }
+
+
         #endregion Properties
         public DiemDTO()
         {
             m_mahs = "";
             m_mamh = "";
-            m_diem = 0;
+            m_diem15 = 0;
+            m_diem1T = 0;
+            m_diemThi = 0;
+            m_diemTB = 0;
         }
 
     }
